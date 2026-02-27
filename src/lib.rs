6b3de2d3b14ad54b6078591cae07cff43c76e604
Kevan Hollbach <khollbach@gmail.com>
2026-02-27 15:10:10 -0500

    Rust version: 2021 -> 2024

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ fn panic(_info: &PanicInfo) -> ! {
 const LOW_RES_SCREEN: *mut u8 = 0x400 as _;
 const TEXT_MODE_ON: *mut u8 = 0xc051 as _;
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 extern "C" fn main() -> ! {
     unsafe { TEXT_MODE_ON.read_volatile() };
     unsafe { LOW_RES_SCREEN.write_volatile(b'A') };
