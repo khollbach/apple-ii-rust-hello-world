76ae6a62b581442ef43eb94226c1573a34d4d27f
Kevan Hollbach <khollbach@gmail.com>
2026-03-03 13:09:00 -0500

    Game of Life!
    It's *very* slow...
    
    I wonder why -- could be interesting to re-write in C.

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ mod screen;
 
 use panic_halt as _;
 
-use crate::screen::Point;
+use crate::screen::{Page, Point, Screen};
 
 #[unsafe(no_mangle)]
 extern "C" fn main() -> ! {
@@ -13,10 +13,41 @@ extern "C" fn main() -> ! {
     screen::hires(false);
     screen::clear_lowres();
 
+    let mut screen = Screen::new();
+
+    let mut curr_page = Page::Page1;
+
     let initial_cells = [(19, 12), (20, 11), (20, 12), (20, 13), (21, 11)];
     for (x, y) in initial_cells {
-        Point::new(x, y).write(0xff);
+        let p = Point::new(x, y);
+        screen.draw(curr_page, p, 0xff);
     }
 
-    loop {}
+    loop {
+        let (dx, dy) = screen.dims_xy();
+        for y in 0..dy {
+            for x in 0..dx {
+                let p = Point::new(x, y);
+                let alive = screen.read(curr_page, p) != 0;
+                let n = count_live_neighbors(&screen, curr_page, p);
+                let should_live = if alive { n == 2 || n == 3 } else { n == 3 };
+
+                let pixel = if should_live { 0xff } else { 0 };
+                screen.draw(curr_page.other(), p, pixel);
+            }
+        }
+
+        curr_page = curr_page.other();
+        screen.select_page(curr_page);
+    }
+}
+
+fn count_live_neighbors(screen: &Screen, page: Page, p: Point) -> u8 {
+    let mut count = 0;
+    for p2 in p.neighbors() {
+        if screen.read(page, p2) != 0 {
+            count += 1;
+        }
+    }
+    count
 }
--- src/screen.rs
+++ src/screen.rs
@@ -1,5 +1,67 @@
 use crate::mem;
 
+// todo: maybe there's a clean way to wrap up the soft-switches
+// in a safe API?
+
+// (low-res, for now)
+pub struct Screen;
+
+impl Screen {
+    pub fn new() -> Self {
+        Self
+    }
+
+    pub fn dims_xy(&self) -> (u8, u8) {
+        (40, 24)
+    }
+
+    pub fn curr_page(&self) -> Page {
+        let byte = unsafe { mem::read(0xC01C) };
+        let flag = byte & 0x8 != 0;
+        if flag { Page::Page2 } else { Page::Page1 }
+    }
+
+    pub fn select_page(&mut self, page: Page) {
+        match page {
+            Page::Page1 => unsafe { mem::read(0xC054) },
+            Page::Page2 => unsafe { mem::read(0xC055) },
+        };
+    }
+
+    pub fn read(&self, page: Page, point: Point) -> u8 {
+        let mut addr = point.addr();
+        if page == Page::Page2 {
+            addr = unsafe { addr.offset(0x400) };
+        }
+
+        unsafe { addr.read_volatile() }
+    }
+
+    pub fn draw(&mut self, page: Page, point: Point, value: u8) {
+        let mut addr = point.addr();
+        if page == Page::Page2 {
+            addr = unsafe { addr.offset(0x400) };
+        }
+
+        unsafe { addr.write_volatile(value) };
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Page {
+    Page1,
+    Page2,
+}
+
+impl Page {
+    pub fn other(self) -> Self {
+        match self {
+            Page::Page1 => Page::Page2,
+            Page::Page2 => Page::Page1,
+        }
+    }
+}
+
 /// Enable text mode.
 pub fn text(enable: bool) {
     if enable {
@@ -28,8 +90,10 @@ pub fn hires(enable: bool) {
 }
 
 /// Write a black pixel to all low-res graphics memory.
+/// 
+/// (both pages)
 pub fn clear_lowres() {
-    for i in 0x400..0x800 {
+    for i in 0x400..0xc00 {
         unsafe { mem::write(i, 0) }
     }
 }
@@ -38,6 +102,7 @@ pub fn clear_lowres() {
 ///
 /// Note that there's technically a "top-half" and a "bottom-half" to each of
 /// these "pixels", in GR mode. Each can hold a 4-bit color.
+#[derive(Debug, Clone, Copy)]
 pub struct Point {
     x: u8,
     y: u8,
@@ -50,12 +115,12 @@ impl Point {
         Self { x, y }
     }
 
-    pub fn read(self) -> u8 {
-        unsafe { self.addr().read_volatile() }
+    pub fn x(self) -> u8 {
+        self.x
     }
 
-    pub fn write(self, value: u8) {
-        unsafe { self.addr().write_volatile(value) }
+    pub fn y(self) -> u8 {
+        self.y
     }
 
     /// What is the memory address for this low-res pixel?
@@ -76,4 +141,29 @@ impl Point {
         let addr: u16 = base + offset + x;
         addr as _
     }
+    
+    pub fn neighbors(self) -> impl Iterator<Item=Point> {
+        let x = self.x as i8;
+        let y = self.y as i8;
+
+        [-1, 0, 1].into_iter().flat_map(move |dy| {
+            [-1, 0, 1].into_iter().filter_map(move |dx| {
+                if (dx, dy) == (0, 0) {
+                    return None;
+                }
+
+                let x2 = x + dx;
+                let y2 = y + dy;
+
+                if !in_bounds(x2, y2) {
+                    return None;
+                }
+                Some(Point::new(x2 as u8, y2 as u8))
+            })
+        })
+    }
+}
+
+fn in_bounds(x: i8, y: i8) -> bool {
+    0 <= x && x < 40 && 0 <= y && y < 24
 }
